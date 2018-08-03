1dab70e98a147869af1706cd4fdc0f90d7362183
Unknown <gaelle_hardy1@hotmail.com>
2018-08-03 17:18:43 +0200

    to do app

---
+++ src/components/TodoList.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <p>Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
+        <p>Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
+        <todo v-on:delete-todo="deleteTodo" v-on:complete-todo="completeTodo" v-for="todo in todos" :todo.sync="todo"></todo>
+    </div>
+</template>
+
+<script type="text/javascript">
+import swa from 'sweetalert';
+import Todo from './Todo';
+
+export default {
+    props: ['todos'],
+    components: {
+        Todo
+    },
+    methods: {
+        deleteTodo(todo) {
+            const todoIndex = this.todos.indexOf(todo);
+            this.todos.splice(todoIndex, 1);
+            swa('Deleted!', 'Your To-Do has been deleted', 'success');
+            
+        },
+        completeTodo(todo) {
+            const todoIndex = this.todos.indexOf(todo);
+            this.todos[todoIndex].done = true;
+            swa('Success!', 'To-Do completed!', 'success');
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
